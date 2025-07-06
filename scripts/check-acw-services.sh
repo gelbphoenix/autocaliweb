e311b3b4fdcd15a0506e658f22a7fffb0adcfd9f
vhsdream <punk.sand7393@fastmail.com>
2025-07-06 18:13:47 -0400

    Manual install: add automations
    - adds the necessary scripts and systemd services to have working
    automations in a manual install
    - Makes one change to a script shared by Docker and non-Docker installs
    - add metadata.db copy command to library dir

--- scripts/check-acw-services.sh
+++ scripts/check-acw-services.sh
@@ -4,39 +4,43 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-# Print promt title
+if [[ -f /app/.no_docker ]]; then
+  INGESTOR_CHECK="$(systemctl is-active acw-ingestor)"
+  DETECTOR_CHECK="$(systemctl is-active acw-change-detector)"
+fi
+
+# Print prompt title
 echo "====== Autocaliweb -- Status of Monitoring Services ======"
 echo ""
 
-
-if s6-rc -a list | grep -q 'acw-ingest-service'; then
-    echo -e "- acw-ingest-service ${GREEN}is running${NC}"
-    is=true
+if [ "$INGESTOR_CHECK" == "active" ] || s6-rc -a list >/dev/null 2>&1 | grep -q 'acw-ingest-service'; then
+  echo -e "- acw-ingest-service ${GREEN}is running${NC}"
+  is=true
 else
-    echo -e "- acw-ingest-service ${RED}is not running${NC}"
-    is=false
+  echo -e "- acw-ingest-service ${RED}is not running${NC}"
+  is=false
 fi
 
-if s6-rc -a list | grep -q 'metadata-change-detector'; then
-    echo -e "- metadata-change-detector ${GREEN}is running${NC}"
-    mc=true
+if [ "$DETECTOR_CHECK" == "active" ] || s6-rc -a list >/dev/null 2>&1 | grep -q 'metadata-change-detector'; then
+  echo -e "- metadata-change-detector ${GREEN}is running${NC}"
+  mc=true
 else
-    echo -e "- metadata-change-detector ${RED}is not running${NC}"
-    mc=false
+  echo -e "- metadata-change-detector ${RED}is not running${NC}"
+  mc=false
 fi
 
 echo ""
 
 if $is && $mc; then
-    echo -e "Autocaliweb was ${GREEN}successfully installed ${NC}and ${GREEN}is running properly!${NC}"
-    exit 0
+  echo -e "Autocaliweb was ${GREEN}successfully installed ${NC}and ${GREEN}is running properly!${NC}"
+  exit 0
 else
-    echo -e "Autocaliweb was ${RED}not installed successfully${NC}, please check the logs for more information."
-    if [ "$is" = true ] && [ "$mc" = false ] ; then
-        exit 1
-    elif [ "$is" = false ] && [ "$mc" = true ] ; then
-        exit 2
-    else
-        exit 3
-    fi
+  echo -e "Autocaliweb was ${RED}not installed successfully${NC}, please check the logs for more information."
+  if [ "$is" = true ] && [ "$mc" = false ]; then
+    exit 1
+  elif [ "$is" = false ] && [ "$mc" = true ]; then
+    exit 2
+  else
+    exit 3
+  fi
 fi

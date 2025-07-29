5ecbb861332cc9b4d3fc87d015898a609b1c9e07
Usama Khalil <usamafoad@gmail.com>
2025-07-29 21:14:29 +0300

    Always recreate and start ACW services in install script
    Modified manual_install_acw.sh to always create and start ACW-related systemd services, regardless of whether the installation is new or existing. This simplifies service management and ensures services are up-to-date after installation.

--- scripts/manual_install_acw.sh
+++ scripts/manual_install_acw.sh
@@ -1474,25 +1474,18 @@ main() {
     initialize_databases
     set_permissions
 
-    # Only create services if they don't exist or if they've changed
-    if [ "$EXISTING_INSTALLATION" != true ]; then
-        create_systemd_service
-        create_acw_ingest_service
-        create_auto_zipper_service
-        create_metadata_change_detector
-    fi
+    # Always create the services
+    create_systemd_service
+    create_acw_ingest_service
+    create_auto_zipper_service
+    create_metadata_change_detector
 
     # Only run auto_library for new installations
     if [ "$install_SCENARIO" != "with_template" ] && [ "$EXISTING_INSTALLATION" != true ]; then
         run_auto_library
     fi
 
-    # Use appropriate service start method
-    if [ "$EXISTING_INSTALLATION" = true ]; then
-        restart_acw_services
-    else
-        start_acw_services
-    fi
+    start_acw_services
 
     set_acw_permissions
     verify_installation

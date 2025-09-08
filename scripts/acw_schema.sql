767640276195e6a541659b338f7d05008f331236
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-09-08 22:49:16 +0200

    fix: Add ingest_timeout_minutes to acw_settings table
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- scripts/acw_schema.sql
+++ scripts/acw_schema.sql
@@ -42,6 +42,7 @@ CREATE TABLE IF NOT EXISTS acw_settings(
     auto_convert_ignored_formats TEXT DEFAULT "" NOT NULL,
     auto_ingest_ignored_formats TEXT DEFAULT "" NOT NULL,
     auto_ingest_automerge TEXT DEFAULT "new_record" NOT NULL,
+    ingest_timeout_minutes INTEGER DEFAULT 15 NOT NULL,
     auto_metadata_enforcement SMALLINT DEFAULT 1 NOT NULL,
     kindle_epub_fixer SMALLINT DEFAULT 1 NOT NULL,
     auto_backup_epub_fixes SMALLINT DEFAULT 1 NOT NULL,

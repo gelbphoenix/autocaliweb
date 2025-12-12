2e4177f2de76f2f676649b8df806ea99fe216ede
Phoenix Paulina Schmid <paulina@gelbphoenix.de>
2025-12-12 19:55:59 +0100

    Remove caching from service-worker.js (Fixes #119)
    Signed-off-by: Phoenix Paulina Schmid <paulina@gelbphoenix.de>

--- cps/static/js/service-worker.js
+++ cps/static/js/service-worker.js
@@ -1,81 +1,11 @@
-const CACHE_NAME = "autocaliweb-cache";
-const PRECACHE_URLS = [
-    "/",
-    "/static/icon-dark.svg",
-    "/static/icon-light.svg",
-    "/static/css/main.css",
-    "/static/js/main.js",
-];
+self.addEventListener("install", (e) => self.skipWaiting());
 
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then((cache) => cache.addAll(PRECACHE_URLS))
-            .then(() => self.skipWaiting())
-    );
-});
+self.addEventListener("activate", (e) => e.waitUntil(clients.claim()));
 
-self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        caches
-            .keys()
-            .then((names) =>
-                Promise.all(
-                    names.map((name) => {
-                        if (name !== CACHE_NAME) return caches.delete(name);
-                    })
-                )
-            )
-            .then(() => self.clients.claim())
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-    if (e.request.mode === "navigate" || e.request.method !== "GET") {
-        return;
-    }
-
-    if (
-        !e.request.url.startsWith(self.location.origin) ||
-        e.request.url.startsWith(self.location.origin + "/kobo/") ||
-        e.request.url.includes("/login") ||
-        e.request.url.includes("/oauth") ||
-        e.request.url.includes("/metadata")
-    ) {
-        return e.respondWith(fetch(e.request));
-    }
-
-    e.respondWith(
-        caches.match(e.request).then(
-            (cached) =>
-                cached ||
-                fetch(e.request).then((response) => {
-                    if (
-                        response.type === "opaqueredirect" ||
-                        response.status === 302
-                    ) {
-                        return response;
-                    }
-
-                    return caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(e.request, response.clone());
-                        return response;
-                    });
-                })
-        )
-    );
-});
+self.addEventListener("fetch", (e) => e.respondWith(fetch(e.request)));
 
 self.addEventListener("message", (e) => {
-    if (e.data === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
-});
-
-// Neue Event-Listener für automatische Updates
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
+    if (e.data && e.data.type === "SKIP_WAITING") {
         self.skipWaiting();
     }
 });
